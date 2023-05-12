db20ae7ae311826b63bf459576fee172f4deeb78
Aditi Sharma <sharmaaditi9112001@gmail.com>
2023-05-13 02:38:31 +0530

    added

---
+++ Health-Care/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./App.css";
+import BarChart from "./Components/charts";
+
+function App() {
+  return (
+    <div className="App">
+      <BarChart />
+    </div>
+  );
+}
+
+export default App;
