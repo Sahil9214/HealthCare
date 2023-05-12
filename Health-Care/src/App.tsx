06418b012d17a58e3b17292795224b4e18c17065
Sahil9214 <utkarshsinghal369@gmail.com>
2023-05-12 16:26:58 +0530

    Horizontal

--- Health-Care/src/App.tsx
+++ Health-Care/src/App.tsx
@@ -2,12 +2,12 @@ import React from "react";
 
 import "./App.css";
 
+import Tracker from "./Pages/Tracker";
 
 function App() {
   return (
     <div className="App">
-      
-      
+      <Tracker/>
     </div>
   );
 }
