82817b6312eacd3cccee4b255f1666444a141bbd
tambeaditya101 <tambeaditya101@gmail.com>
2023-05-13 23:22:44 +0530

    shoulders done

--- Health-Care/src/Allroutes/Allroutes.tsx
+++ Health-Care/src/Allroutes/Allroutes.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Login from "../Pages/Login";
 import Signup from "../Pages/Signup";
+import Shoulder from "../Pages/Shoulder";
 
 function Allroutes() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
+      <Route path="/shoulder" element={<Shoulder />} />
     </Routes>
   );
 }
